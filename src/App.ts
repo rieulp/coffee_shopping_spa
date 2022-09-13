ebf8f14e49d5ee980370a3d1228de488a734c270
rayoungpark <rayoung1229@gmail.com>
2022-09-13 22:15:33 +0900

    🚧 라우터 기능 연결중
    - 현재 `/` 연결 완료

--- src/App.ts
+++ src/App.ts
@@ -1,20 +1,30 @@
-import { router } from "./router/index";
-
+import ProductListPage from './pages/ProductListPage';
+import { initRouter } from './router/index';
+import 'C:/Users/rayou/Desktop/과제테스트/coffee_shopping_spa/style.css';
 export default class App {
   private state = {};
+  private router;
 
-  constructor(private $app: HTMLDivElement) {
-    this.render();
-    router.init([{ path: "/", view: () => console.log("home") }]);
+  constructor(private $app: Element) {
+    this.router = initRouter($app, [
+      { path: /^\/$/, view: ProductListPage },
+      {
+        path: /^\/products\/[0-9]+$/,
+        view: () => console.log('detail'),
+      },
+      {
+        path: /^\/cart$/,
+        view: () => console.log('cart'),
+      },
+    ]);
+
+    // const homePage = new ProductListPage({ $target: $app, initialState: { products: [] } });
+    // this.router.router();
   }
   setState(nextState: {}) {
     this.state = { ...this.state, ...nextState };
     this.render();
   }
 
-  render() {
-    this.$app.innerHTML = "<h1>blah</h1>";
-
-    // fetchProductList().then((data) => console.log(data));
-  }
+  render() {}
 }
