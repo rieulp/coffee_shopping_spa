8fd7e06472dd79747f5c82378c2c8c3f796eb0da
rayoungpark <rayoung1229@gmail.com>
2022-09-13 22:11:48 +0900

    🔇 remove logs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import App from "./App";
+import App from './App';
 
-const $app = document.querySelector<HTMLDivElement>(".App");
+const $app = document.querySelector<HTMLDivElement>('.App');
 if ($app) new App($app);
-console.log("load");
