59108eea8c443aac69c2c60c7545fec4eb5aa8fb
rayoungpark <rayoung1229@gmail.com>
2022-09-15 00:28:51 +0900

    ⚡️ fetch 데이터 캐시 처리

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,12 @@
+const cache: { [key: string]: any } = {};
+
 export const request = async (url: string, options: RequestInit = {}) => {
   try {
     const response = await fetch(url, options);
     if (response.ok) {
       return await response.json();
     }
-    throw new Error("API 통신 실패");
+    throw new Error('API 통신 실패');
   } catch (e) {
     let message;
     if (e instanceof Error) message = e.message;
@@ -14,11 +16,15 @@ export const request = async (url: string, options: RequestInit = {}) => {
 };
 
 export const fetchProductList = async () => {
+  if (cache.all) return cache.list;
   const data = await request(`api/products`);
+  if (data) cache.list = data;
   return data || [];
 };
 
 export const fetchProduct = async (productId: number) => {
+  if (cache[productId]) return cache[productId];
   const data = await request(`api/products/${productId}`);
+  if (data) cache[productId] = data;
   return data || {};
 };
