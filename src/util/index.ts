cd6aec139306a11cc31d2d08f33d2bc947f729a1
rayoungpark <rayoung1229@gmail.com>
2022-09-15 00:26:13 +0900

    🐛 argument가 undefined일때 예외처리

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,3 +1,6 @@
-const toComma = (price: number | string) => price.toLocaleString('ko-KR');
+const toComma = (price: number | string) => {
+  if (price === undefined) return undefined;
+  return price.toLocaleString('ko-KR');
+};
 
 export { toComma };
