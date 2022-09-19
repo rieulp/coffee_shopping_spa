49157da4a07bfa488201f2c2651cb6cce9558a3d
rayoungpark <rayoung1229@gmail.com>
2022-09-20 01:00:54 +0900

    ✨ 카트 아이템 부분 삭제 기능 추가

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,7 +10,6 @@ interface ICartItem {
 
 const addCart = (productId: number, products: ISelectedOption[]) => {
   const data = getCart();
-  console.log(data);
   products.forEach(({ optionId, quantity }) => {
     const idx = data.findIndex(({ productId: id, optionId: oid }) => id === productId && optionId === +oid);
     if (idx >= 0) data[idx].quantity = quantity;
@@ -23,8 +22,16 @@ const getCart = (): ICartItem[] => {
   return JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '[]');
 };
 
-const removeCart = () => {
-  localStorage.removeItem(LOCAL_STORAGE_KEY);
+const removeCart = (productId?: number, optionId?: number) => {
+  if (productId === undefined || optionId === undefined) localStorage.removeItem(LOCAL_STORAGE_KEY);
+  else {
+    const cart = getCart();
+    const idx = cart.findIndex(({ productId: pid, optionId: oid }) => productId === pid && optionId === oid);
+    if (idx >= 0) {
+      cart.splice(idx, 1);
+      localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(cart));
+    }
+  }
 };
 
 export { addCart, getCart, removeCart };
